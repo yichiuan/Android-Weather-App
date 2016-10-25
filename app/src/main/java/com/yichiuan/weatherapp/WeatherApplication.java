f9236e238466f9e8b375aa7c88bf956a62d522eb
yichiuan <yang.yichiuan@gmail.com>
2016-10-25 23:00:45 +0800

    Open StrictMode in Debug

--- app/src/main/java/com/yichiuan/weatherapp/WeatherApplication.java
+++ app/src/main/java/com/yichiuan/weatherapp/WeatherApplication.java
@@ -1,6 +1,7 @@
 package com.yichiuan.weatherapp;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -9,6 +10,23 @@ public class WeatherApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // StrictMode setup
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectCustomSlowCalls()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build());
+        }
+
         EventBus.builder().addIndex(new EventBusIndex()).installDefaultEventBus();
     }
 }
